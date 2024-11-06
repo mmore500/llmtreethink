57ac411e5c1eec81bc44fd412b5f4e2b958729d6
mmore500 <mmore500.login@gmail.com>
2024-11-06 02:49:42 +0000

    Deploying to binder from @ mmore500/llmtreethink@7c16ecda4dbe0a8148ee5e5aa22ca44cb0d46ccc 🚀

--- pylib/treeprep/_sample_phylogeny_newick.py
+++ pylib/treeprep/_sample_phylogeny_newick.py
@@ -9,11 +9,11 @@ def sample_phylogeny_newick(num_taxa: int) -> str:
         death_rate=0.0,
         num_extant_tips=num_taxa,
     )
+    for node in tree.preorder_node_iter():
+        node.edge.length = None
     newick_str = (
         tree.as_string(schema="newick")
         .replace("[&]", "")
-        .translate({c: None for c in "0123456789"})
-        .replace(":", "")
         .replace(".", "")
     )
     return newick_str
