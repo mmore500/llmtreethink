4d447e965fcb2fa3c95c1b88eefa5027271e3e70
mmore500 <mmore500.login@gmail.com>
2024-11-06 03:36:36 +0000

    Deploying to binder-2024-10-20-example.ipynb from @ mmore500/llmtreethink@4f7b165b8d130e223d67a8ab8072f36ca4831ee7 🚀

--- pylib/_auxlib/_swap_words.py
+++ pylib/_auxlib/_swap_words.py
@@ -1,7 +1,11 @@
 # adapted from https://stackoverflow.com/a/70209661/17332200
 def swap_words(source: str, first: str, second: str) -> str:
-    res = second.join(
-        part.replace(second, first) for part in source.split(first)
+    res = (
+        source
+        .replace(first, chr(0))
+        .replace(second, first)
+        .replace(chr(0), second)
     )
+
     assert sorted(res) == sorted(source)
     return res
