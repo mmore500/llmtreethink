24debb29bdaf69153ac370f8250679ae99acae2b
mmore500 <mmore500.login@gmail.com>
2024-11-06 03:51:18 +0000

    Deploying to binder-2024-10-20-example.ipynb from @ mmore500/llmtreethink@bcf8077bbc55f2822e1b5623e05af59103c5911f 🚀

--- pylib/query/_query_openai.py
+++ pylib/query/_query_openai.py
@@ -1,3 +1,4 @@
+import time
 import typing
 
 import openai
@@ -14,10 +15,19 @@ def query_openai(
 
     assert 0 <= true_answer < len(choices)
 
-    response = openai.ChatCompletion.create(
-        model=model,  # Specify your desired model
-        messages=[{"role": "user", "content": prompt}],
-    )
+    for retry in range(10):
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=[{"role": "user", "content": prompt}],
+            )
+            break
+
+        except openai.error.RateLimitError:
+            print(f"Rate limit error, retrying in {retry} minutes...")
+            time.sleep(60 * retry)
+    else:
+        raise RuntimeError("Rate limit error, too many retries")
 
     # Extract the response from the LLM
     llm_answer_raw = response["choices"][0]["message"]["content"].strip()
