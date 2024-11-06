33b8387790f4a034eab4df3418479de34e1ff98a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-05 22:42:42 -0500

    Shuffle leaf names, use zfill to ensure unique

--- pylib/treeprep/_sample_phylogeny_newick.py
+++ pylib/treeprep/_sample_phylogeny_newick.py
@@ -1,3 +1,5 @@
+import random
+
 import dendropy as dp
 
 
@@ -9,6 +11,13 @@ def sample_phylogeny_newick(num_taxa: int) -> str:
         death_rate=0.0,
         num_extant_tips=num_taxa,
     )
+
+    leaves = [*tree.leaf_node_iter()]
+    num_leaves = len(leaves)
+    random.shuffle(leaves)
+    for i, node in enumerate(leaves):
+        node.taxon.label = "T" + str(i).zfill(len(str(num_leaves)))
+
     for node in tree.preorder_node_iter():
         node.edge.length = None
     newick_str = (
