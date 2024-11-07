6c6504664d2d3a33be068c1a0a1112ae4e6b9d3a
mmore500 <mmore500.login@gmail.com>
2024-11-07 17:55:58 +0000

    Deploying to binder from @ mmore500/llmtreethink@91ad0c0308face5eb188e55f20476c129d55c69f 🚀

--- slurm/2024-11-07/2024-11-07-scientific.sh
+++ slurm/2024-11-07/2024-11-07-scientific.sh
@@ -195,7 +195,6 @@ cat > "${SBATCH_FILE}" << EOF
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --time=24:00:00
-#SBATCH --constraint=amr
 #SBATCH --output="/mnt/home/%u/joblog/%j"
 #SBATCH --mail-user=mawni4ah2o@pomail.net
 #SBATCH --mail-type=FAIL,TIME_LIMIT
