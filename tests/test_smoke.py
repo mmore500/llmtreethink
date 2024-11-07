563014cf09bb75fc0b084a6ad3dcdc4578ad3eab
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-07 11:23:19 -0500

    Fix tidy lint

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -11,8 +11,10 @@ from pylib.prompt._question_equivalence_swap_taxa import (
 from pylib.prompt._question_most_related import question_identify_most_related
 from pylib.prompt._question_most_related_to_x import question_most_related_to_x
 from pylib.query._query_openai import query_openai
-from pylib.treeprep._scientific_phylogeny_newick import scientific_phylogeny_newick
 from pylib.treeprep._sample_phylogeny_newick import sample_phylogeny_newick
+from pylib.treeprep._scientific_phylogeny_newick import (
+    scientific_phylogeny_newick,
+)
 from pylib.treerepr._make_json_from_newick import make_json_from_newick
 
 
