fcfad545ea711c45c37021f8a9cee8b034d17752
mmore500 <mmore500.login@gmail.com>
2024-11-08 05:09:23 +0000

    Deploying to binder-2024-11-05-openai-repr-size-4omini.ipynb from @ mmore500/llmtreethink@b1a4b2c78499e7dd6177dfdb6dc40bb43daebb7e 🚀

--- pylib/treeprep/_shuffle_taxa_newick.py
+++ pylib/treeprep/_shuffle_taxa_newick.py
@@ -16,11 +16,16 @@ def shuffle_taxa_newick(newick_str: str) -> str:
     for __ in range(10000):
         leaves = [*tree.leaf_node_iter()]
 
-        for i in range(0, len(leaves)-2):
-            j = random.randint(i, len(leaves)-1)
+        for i in range(0, len(leaves) - 2):
+            j = random.randint(i, len(leaves) - 1)
             leaves[i].taxon, leaves[j].taxon = leaves[j].taxon, leaves[i].taxon
 
-            if dp.calculate.treecompare.weighted_robinson_foulds_distance(orig_tree, tree) > 0:
+            if (
+                dp.calculate.treecompare.weighted_robinson_foulds_distance(
+                    orig_tree, tree
+                )
+                > 0
+            ):
                 for node in tree.preorder_node_iter():
                     node.edge.length = None
                 return tree.as_string(schema="newick").replace(".", "")
