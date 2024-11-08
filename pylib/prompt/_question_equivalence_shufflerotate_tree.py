7e0dd51ee8d3070edccac41e082522ac5d5e4e4e
mmore500 <mmore500.login@gmail.com>
2024-11-08 03:11:42 +0000

    Deploying to binder-2024-11-07-scientific.ipynb from @ mmore500/llmtreethink@43d49a2e743359c301fe34b63bb176b39794261c 🚀

---
+++ pylib/prompt/_question_equivalence_shufflerotate_tree.py
@@ -0,0 +1,32 @@
+import random
+import typing
+
+import dendropy as dp
+
+from ..treeprep._rotate_phylogeny_newick import rotate_phylogeny_newick
+
+
+def question_equivalence_shufflerotate_tree(
+    newick_string: str,
+    taxa: typing.Optional[typing.Sequence[str]] = None,
+) -> tuple[str, list[str], int, list[str]]:
+
+    question = (
+        "Do the two given phylogenies represent the same tree structure?"
+    )
+    choices = [
+        "yes, the phylogenies are equivalent",
+        "no, the phylogenies are different",
+    ]
+
+    answer = 0
+
+    tree = dp.Tree.get(data=newick_string, schema="newick")
+    for node in tree.preorder_node_iter():
+        child_nodes = node.child_nodes()
+        random.shuffle(child_nodes)
+        node.clear_child_nodes()
+        node.set_child_nodes(child_nodes)
+
+    rotated_newick_string = rotate_phylogeny_newick(tree.as_string("newick"))
+    return question, choices, answer, [newick_string, rotated_newick_string]
